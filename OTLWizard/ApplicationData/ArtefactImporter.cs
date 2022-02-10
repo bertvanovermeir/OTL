4c5fc785a801d89406e999cfcb33c7006eee879e
bert.vanovermeir@gmail.com <bert.vanovermeir@gmail.com>
2022-02-10 17:34:08 +0100

    added artefact query and classes

--- OTLWizard/ApplicationData/ArtefactImporter.cs
+++ OTLWizard/ApplicationData/ArtefactImporter.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,16 +12,30 @@ namespace OTLWizard.ApplicationData
     public class ArtefactImporter
     {
         private SQLiteConnection sqlite_conn_OTL;
+        private ApplicationManager app;
         public List<OTL_ArtefactType> OTL_ArtefactTypes;
 
-        public ArtefactImporter(string dbpath)
+        public ArtefactImporter(string dbpath, ApplicationManager app)
         {
+            this.app = app;
             sqlite_conn_OTL = new SQLiteConnection("Data Source = " + dbpath + "; Version = 3; Read Only = True;");
         }
 
         public void ParseDatabase()
         {
-            string query = "select * from *";
+            string query="";
+            // open the queries file (queries in known order in file)
+            if (File.Exists(Directory.GetCurrentDirectory() + "\\queries.dat"))
+            {
+                string[] lines = File.ReadAllLines(Directory.GetCurrentDirectory() + "\\queries.dat", System.Text.Encoding.UTF8);
+                query = lines[3];
+
+            }
+            else
+            {
+                // failure to open will generate a message for now, no further action
+                app.showMessage("Could not retrieve Query information.", "Fatal Error");
+            }
             // open the connection:
             sqlite_conn_OTL.Open();
             var sqlite_cmd = sqlite_conn_OTL.CreateCommand();
@@ -30,8 +45,7 @@ namespace OTLWizard.ApplicationData
             while (sqlite_datareader.Read()) // Read() returns true if there is still a result line to read
             {
                 // check columns in query to know what to transfer, 
-                OTL_ArtefactType artefact = new OTL_ArtefactType();
-                // OTL_ObjectType temp = new OTL_ObjectType((string)sqlite_datareader.GetValue(1), (string)sqlite_datareader.GetValue(0), (string)sqlite_datareader.GetValue(3), (string)sqlite_datareader.GetValue(2));
+                OTL_ArtefactType artefact = new OTL_ArtefactType((string)sqlite_datareader.GetValue(0), (string)sqlite_datareader.GetValue(1), (string)sqlite_datareader.GetValue(2), (string)sqlite_datareader.GetValue(3), (string)sqlite_datareader.GetValue(4), (string)sqlite_datareader.GetValue(5), (string)sqlite_datareader.GetValue(6), (string)sqlite_datareader.GetValue(7));
                 OTL_ArtefactTypes.Add(artefact);
             }
             sqlite_conn_OTL.Close();
--- OTLWizard/OTLObjecten/OTL_ArtefactType.cs
+++ OTLWizard/OTLObjecten/OTL_ArtefactType.cs
@@ -8,7 +8,26 @@ namespace OTLWizard.OTLObjecten
 {
     public class OTL_ArtefactType
     {
+        public string objectnaam;
+        public string geometrie;
+        public string overerving;
+        public string uitzonderingen;
+        public string overervingsgrens;
+        public string steekkaarten;
+        public string overervenvan;
+        public string viarelatie;
 
+        public OTL_ArtefactType(string objectnaam, string geometrie, string overerving, string uitzonderingen, string overervingsgrens, string steekkaarten, string overervenvan, string viarelatie)
+        {
+            this.objectnaam = objectnaam;
+            this.geometrie = geometrie;
+            this.overerving = overerving;
+            this.uitzonderingen = uitzonderingen;
+            this.overervingsgrens = overervingsgrens;
+            this.steekkaarten = steekkaarten;
+            this.overervenvan = overervenvan;
+            this.viarelatie = viarelatie;
+        }
 
 
     }
