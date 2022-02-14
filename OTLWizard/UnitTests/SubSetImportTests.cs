ea956fe8db039f7546bf2c11ff2e7ddeedd9a224
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-02-14 10:59:16 +0100

    adding test files

---
+++ OTLWizard/UnitTests/SubSetImportTests.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace OTLWizard.UnitTests
+{
+    public class SubSetImportTests
+    {
+        [Fact]
+        public void SubsetImportTest_1_type()
+        {
+            // arrange
+            var dbpath = Directory.GetCurrentDirectory() + "\\UnitTests\\test_1_type.db";
+            var subsetImporter = new SubsetImporter(dbpath, "", null);
+            
+            // act
+            subsetImporter.ImportSubset();
+            
+            // assert
+            var objectTypes = subsetImporter.GetOTL_ObjectTypes();
+            Assert.Equal(1, objectTypes.Count);
+            var objectType = objectTypes.FirstOrDefault();
+            Assert.Equal("https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#DNBLaagspanning", objectType.uri);
+            
+        }
+    }
+}
