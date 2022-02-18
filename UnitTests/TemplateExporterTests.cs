7b7dbd542f1519e1137cccb02f7b4f81321d09a0
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-02-18 16:05:34 +0100

    template exporter test

---
+++ UnitTests/TemplateExporterTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Microsoft.Office.Interop.Excel;
+using Xunit;
+using OTLWizard.Helpers;
+
+namespace UnitTests
+{
+    public class TemplateExporterTests
+    {
+        [Fact]
+        public void ArtefactImportTest_3_types()
+        {
+            // arrange           
+            var otldbpath = "./../../subset_3_types_netwerk.db";
+            var subsetImporter = new SubsetImporter(otldbpath);
+            var path_save_to = "test_result.xlsx";
+            
+            // act
+            subsetImporter.Import();
+            var exporter = new TemplateExporter(subsetImporter.GetOTLObjectTypes());
+            exporter.ExportXls(path: Directory.GetCurrentDirectory() + "\\" + path_save_to, help: false, checklistoptions:false);
+ 
+            // assert
+            Application excel;
+            Workbook workbook;
+            excel = new Application {Visible = false, DisplayAlerts = false};
+            workbook = excel.Workbooks.Open(Directory.GetCurrentDirectory() + "\\" + path_save_to);
+            var xlWorkSheet = (Worksheet) workbook.Worksheets["Netwerkpoort"];
+            Assert.NotNull(xlWorkSheet);
+            workbook.Close();
+            excel.Quit();
+        }
+    }
+}
